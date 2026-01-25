420a985dcd07d94a1a25b5c6ad6592bba7647990
Aryan Singh <aryanbaghi076@gmail.com>
2026-01-25 15:36:55 +0530

    added somethin

---
+++ chapter3..c/for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int i,n;
+    printf("Enter an no. n: ");
+    scanf("%d", &n);
+
+    for(i=1; i<=n; i++){
+        printf("The natural no(s) are : %d\n", i);
+    };
+    return 0;
+}

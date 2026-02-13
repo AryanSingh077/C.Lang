35e87564d02a2b286871a485c5295c5e06656647
Aryan Singh <aryanbaghi076@gmail.com>
2026-02-13 14:49:42 +0530

    added somethin

---
+++ strings.c
@@ -0,0 +1,9 @@
+// A string is an array of characters terminated by null character arrays.
+
+// some strings handling functions: from <string.h>
+/* strlen(str) for length
+   strcpy(str1,str2) to copy string
+   strcat(str1,str2) to concatenate
+   strcmp(s1,s2) to compare
+   strchr(str, ch) find character
+   strstr(str,sub) find substring */

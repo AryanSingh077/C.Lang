f3c2bb48e34c3754ee1dcca7baf4afd401bd6a7c
Aryan Singh <aryanbaghi076@gmail.com>
2026-03-12 23:27:39 +0530

    added some more ques

--- assingment2_C/avvveee.c
+++ assingment2_C/avvveee.c
@@ -39,21 +39,115 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main(){
+
+//     int i,j,k, rows;
+//     rows = 5;
+
+//     for(i=0;i<=rows;i++){
+//         for(j=0;j<=rows-i;j++){
+//             printf(" ");
+//         };
+//         for(k=0;k<i;k++){
+//             printf("%c ", 'A'+k);
+//         };
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// GCD:
+// greatest no. which divides two integers...
+// like 36 & 8 has GCD: 4;
+// now what about big no.s for them we have Euclidean Algorithm:
+// which states that, the GCD of two no.s will give 0 in remainder, when the remainder is divider by the the smaller no.
+// ex: a = 100, b = 45
+// when a%b = 10; then -> b%10 = 5 then -> 10%5 = 0 means-> 5 is the GCD:
+
+// now applying this logic in C:
+// #include <stdio.h>
+
+// #include <stdio.h>
+// int main(){
+//     int a,b, rem;
+//     printf("Enter your no.s a & b: \n");
+//     scanf("%d%d", &a, &b);
+//     while(b != 0){
+//         rem = a%b;
+//         a = b;
+//         b = rem;
+//     }
+//     printf("The GCD of a and b is: %d", a);
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int gcd(int a, int b){
+//     if(b==0){
+//         return a;
+//     }
+//     else{
+//         return gcd(b,a%b);
+//     };
+// };
+
+// int main() {
+//     int a, b, result;
+//     printf("Enter two numbers a and b\n");
+//     scanf("%d%d", &a, &b);
+//     result = gcd(a, b);
+//     printf("The GCD is: %d", result);
+//     return 0;
+// }
+
+// now we will move to check matrix:
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// int main(){
+//     bool isDiagonal=true, isUpper=true, isLower=true;
+//     int mat[3][3];
+
+//     for(int i = 0;i<3;i++){
+//         for(int j = 0;j<3;j++){
+//             printf("Fill your matrix where i = %d and j = %d\n", i,j);
+//             scanf("%d", &mat[i][j]);
+//         }
+//     }
+
+//     for(int i = 0; i < 3 ; i++){
+//         for(int j = 0; j < 3 ; j++){
+//             if(i>j && mat[i][j]!=0){
+//                 isUpper = false;
+//             }
+
+//             if(i<j && mat[i][j]!=0){
+//                 isLower = false;
+//             }
+//         }
+//     }
+
+//     if(isUpper && isLower){
+//         printf("The matrix is Diagonal matrix\n");
+//     }
+//     else if(isUpper){
+//         printf("The matrix is Upper Diagonal Matrix\n");
+//     }
+//     else if(isLower){
+//         printf("The matrix is Lower Diagonal matrix\n");
+//     } 
+//     else{
+//         printf("The matrix is neither Triangular nor diagonal\n");
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(){
-
-    int i,j,k, rows;
-    rows = 5;
-
-    for(i=0;i<=rows;i++){
-        for(j=0;j<=rows-i;j++){
-            printf(" ");
-        };
-        for(k=0;k<i;k++){
-            printf("%c ", 'A'+k);
-        };
-        printf("\n");
-    }
-    return 0;
-}
+// meow meow meow...
